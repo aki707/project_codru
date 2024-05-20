33b62d298cb0a7dfd3140f30ef9c91f93e15b3fe
Aaryan Vijayvargiya <111130488+Aaryanvijay12345@users.noreply.github.com>
2024-05-20 09:43:18 +0530

    File renamed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Home} />
       <Route path="/signup" Component={Signup}/>
+      <Route path="/signin" Component={Signin}/>
       
     </Routes>
   );
