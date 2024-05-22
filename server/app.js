d1ceced4da9de57afceaac580375ca847392d778
Anirudh Pratap Singh <iamaniruddh2003@gmail.com>
2024-05-22 10:49:29 +0530

    blogs api

--- server/app.js
+++ server/app.js
@@ -67,36 +67,4 @@ app.listen(port, () => {
   console.log(`Server is listening at ${port}`);
 });
 
-app.post("/blogs", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return res.status(400).json({ error: "Empty field(s)" });
-    }
-
-    const bloggerLogin = await Blogger.findOne({ username: username });
-
-    if (bloggerLogin) {
-      const isMatched = await bcrypt.compare(password, studentLogin.password);
-      const token = await bloggerLogin.generateAuthToken();
-      console.log(token);
-
-      res.cookie("jwtoken", token, {
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-      });
-
-      if (!isMatched) {
-        res.status(400).json({ error: "Wrong Credentials" });
-      } else {
-        res.json({ message: "You are in", bloggerLogin });
-      }
-    } else {
-      res.status(400).json({ error: "Wrong Credentials" });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
 // const s = require("crypto").randomBytes(64).toString("hex");
