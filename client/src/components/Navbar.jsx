61f6a7fbf89a1b068428c65eb6250805d1b47df8
Anirudh Pratap Singh <iamaniruddh2003@gmail.com>
2024-05-23 08:46:24 +0530

    merging

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 // import c3 from "../assets/c3.png"
 
 
-import c3 from "../assets/c3.png";
+import c4 from "../assets/c4.png";
 import "../styles/Navbar.css";
 
 
@@ -19,7 +19,7 @@ function Navbar() {
       <div className="navlogo">
         <img
           //   src="https://cdn.pixabay.com/photo/2023/04/05/16/08/hedgehog-fly-7901862_640.jpg"
-          src={c3}
+          src={c4}
           alt=""
         />
 
