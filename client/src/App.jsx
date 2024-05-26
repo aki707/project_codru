3312f7ebb19d4a8545c11b422e9d3f0f5e2cdbaa
Futurestar7891 <gajanandmali69@gmail.com>
2024-05-26 11:42:52 +0530

    mergin commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={Home} />
+      <Route path="/login" Component={Login} />
     </Routes>
   );
 }
