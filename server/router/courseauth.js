fd990898c166b835e98eebe0e46a9906759a0d47
Anirudh Pratap Singh <iamaniruddh2003@gmail.com>
2024-05-28 10:49:07 +0530

    added courseSchema

---
+++ server/router/courseauth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const Course = require("../models/courseSchema");
+const router = express.Router();
+
+route.post("/add-courses", async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router;
