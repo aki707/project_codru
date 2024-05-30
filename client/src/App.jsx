26e328473546e343d37d07f1f98d19b5c7618d71
Aaryan Vijayvargiya <111130488+Aaryanvijay12345@users.noreply.github.com>
2024-05-30 18:34:25 +0530

    Courses File Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,10 @@ import Home from "./components/Home";
 import Popup from "./components/Popup";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
-// import Login from "./components/Login";
+import Courses from './components/Courses.jsx';
+import Extra from './components/Extra.jsx';
+// import Class11 from './components/Class11.jsx';
+import Class6 from './components/Class6.jsx';
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
       <Route path="/contact" Component={Contact} />
       <Route path="/course-register" Component={Admission} />
       <Route path="/About" Component={About}/>
+      <Route path='/courses' Component={Courses}/>
+      <Route path='/extra' Component={Extra}/>
+     <Route path='/class6' Component={Class6}/>
     </Routes>
   );
 }
