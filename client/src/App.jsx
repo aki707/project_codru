dd3217bb8c861a05ac6dbc1fbc33993a5229aa88
Aaryan Vijayvargiya <111130488+Aaryanvijay12345@users.noreply.github.com>
2024-06-04 10:58:44 +0530

    Signin Page Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,38 +12,24 @@ import Home from "./components/Home";
 import Popup from "./components/Popup";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
-<<<<<<< HEAD
-import Profile from "./components/Profile";
-// import Login from "./components/Login";
-=======
->>>>>>> d39f582d681eb883e9aa63b0c8b7ea230b7cdaa0
 
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
-      {/* <Route path="/" Component={Home} /> */}
-=======
       <Route path="/" Component={Home} />
       <Route path="/forgot-password" Component={Forget_password} />
       <Route path="/change-password" Component={Change_password}/>
->>>>>>> d39f582d681eb883e9aa63b0c8b7ea230b7cdaa0
       <Route path="/signup" Component={Signup} />
       <Route path="/signin" Component={Signin} />
       <Route path="/popup" Component={Popup} />
       <Route path="/contact" Component={Contact} />
       <Route path="/course-register" Component={Admission} />
-<<<<<<< HEAD
-      <Route path="/About" Component={About} />
-      <Route path="/" Component={Profile} />
-=======
       <Route path="/About" Component={About}/>
       <Route path='/courses' Component={Courses}/>
       {/* <Route path='/extra' Component={Extra}/> */}
      {/* <Route path='/class6' Component={Class6}/> */}
      {/* <Route path='/buy' Component={Buy}/> */}
     
->>>>>>> d39f582d681eb883e9aa63b0c8b7ea230b7cdaa0
     </Routes>
   );
 }
