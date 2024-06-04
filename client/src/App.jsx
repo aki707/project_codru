6b0f4a2578fe195eaf59757c26ffe6e570f0ca49
gopeshkumarjangid <gopeshsharma594@gmail.com>
2024-06-04 08:56:58 +0530

    add forgot page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Admission from "./components/Admission";
+// import Buy from './components/Buy.jsx';
+import Change_password from "./components/Change_password";
+// import Class6 from './components/Class6.jsx';
 import Contact from "./components/Contact";
+import Courses from './components/Courses.jsx';
+// import Extra from './components/Extra.jsx';
+import Forget_password from "./components/Forget_password";
 import Home from "./components/Home";
 import Popup from "./components/Popup";
 import Signin from "./components/Signin";
-import Forget_password from "./components/Forget_password";
 import Signup from "./components/Signup";
-import Change_password from "./components/change_password";
-import Courses from './components/Courses.jsx';
-import Class6 from './components/Class6.jsx';
-import Extra from './components/Extra.jsx';
-import Buy from './components/Buy.jsx';
 
 function App() {
   return (
@@ -26,9 +26,9 @@ function App() {
       <Route path="/course-register" Component={Admission} />
       <Route path="/About" Component={About}/>
       <Route path='/courses' Component={Courses}/>
-      <Route path='/extra' Component={Extra}/>
-     <Route path='/class6' Component={Class6}/>
-     <Route path='/buy' Component={Buy}/>
+      {/* <Route path='/extra' Component={Extra}/> */}
+     {/* <Route path='/class6' Component={Class6}/> */}
+     {/* <Route path='/buy' Component={Buy}/> */}
     
     </Routes>
   );
