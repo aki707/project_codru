c88917074edc04ae9361bd2584c4b067bcf84d1e
Anirudh Pratap Singh <iamaniruddh2003@gmail.com>
2024-06-10 09:01:54 +0530

    add profile

--- server/router/userauth.js
+++ server/router/userauth.js
@@ -416,4 +416,21 @@ router.post("/profile-edit", async (req, res) => {
   }
 });
 
+router.get("/profile", async (req, res) => {
+  try {
+    const {username} = req.body;
+
+    let user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Le profile", user: user });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
