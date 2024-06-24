4f7866e2353410000665ab6f7110d36e7d065cf4
Aaryan Vijayvargiya <111130488+Aaryanvijay12345@users.noreply.github.com>
2024-06-25 04:06:39 +0530

    Files Added

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,51 @@
+// import React, { useState } from 'react';
+// import c4 from '../assets/c4.png';
+// import "../styles/Dashboard.css"; 
+
+
+
+
+
+
+
+// const Dashboard = () => {
+//   const [date, setDate] = useState(new Date());
+
+//   return (
+//     <div className="mainDashboard">
+//       <div className="leftSectionDashboard">
+//         <img src={c4} alt="" />
+
+//         <div className="photoDashboard">
+//           <div
+//             className="pDashboard">
+             
+//             </div>
+
+//           <h3
+//             style={{
+//               color: "white",
+//               width: "75%",
+//               marginLeft: "25px",
+//               marginTop: "20px",
+//             }}
+//           >
+//             Aaryan Vijay
+//           </h3>
+//         </div>
+//       </div>
+
+//       <div className="middleSectionDashboard">
+//         <div className="nameDashboard">
+//           <h2>Welcome Aaryan Vijayvargiya</h2>
+//         </div>
+//         <div className="calenderDashboard">
+        
+//         </div>
+//         <div className="eventsSectionDashboard"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
