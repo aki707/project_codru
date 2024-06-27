a4b81ede4a40aff512d38d3fe38e7b3261a6a716
yudhi50 <154526426+yudhi50@users.noreply.github.com>
2024-06-27 19:50:18 +0530

    Create PlanetryAnimatedBackground.jsx
    Animated Background

---
+++ client/src/components/PlanetryAnimatedBackground.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/AnimatedBackground.css';
+
+const PlanetryAnimatedBackground = () => {
+  return (
+  
+    <div 
+     className="Planet_Background"> </div> 
+    
+     
+  );
+};
+
+export default PlanetryAnimatedBackground;
